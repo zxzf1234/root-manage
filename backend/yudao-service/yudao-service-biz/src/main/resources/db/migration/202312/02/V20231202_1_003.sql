ecdbb8daf3caca44cd45430885f0e9bf9963e111
zhaoxu <1162291474@qq.com>
2023-12-02 14:44:58 +0800

    优化模块中字典数据、类型命名

---
+++ backend/yudao-service/yudao-service-biz/src/main/resources/db/migration/202312/02/V20231202_1_003.sql
@@ -0,0 +1,2 @@
+update INFRA_INTERFACE_MODULE set UPDATER_ID = '1', NAME = 'dictData', COMMENT = '字典数据', PARENT_ID = '0fde120d-ce5d-48e3-ad64-285c8ef82839', TYPE = '1' where ID = '82ef8482-f1b9-4a83-bd32-e2292cca653e';
+update INFRA_INTERFACE_MODULE set UPDATER_ID = '1', NAME = 'dictType', COMMENT = '字典类型', PARENT_ID = '0fde120d-ce5d-48e3-ad64-285c8ef82839', TYPE = '1' where ID = 'acdb3e01-5e3d-476b-bf2d-86619fbd40ae';
