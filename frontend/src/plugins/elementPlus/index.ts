0d5bfd73dff862ca012e4841f10bac2c04357b07
zhaoxu <1162291474@qq.com>
2024-03-28 09:47:08 +0800

    element-plus从部分引入改成完整引入

--- frontend/src/plugins/elementPlus/index.ts
+++ frontend/src/plugins/elementPlus/index.ts
@@ -1,34 +1,7 @@
 import type { App } from 'vue'
-// 需要全局引入一些组件，如ElScrollbar，不然一些下拉项样式有问题
-import {
-  ElLoading,
-  ElScrollbar,
-  ElButton,
-  ElTag,
-  ElSpace,
-  ElDropdown,
-  ElDropdownItem,
-  ElDropdownMenu
-} from 'element-plus'
-
-const plugins = [ElLoading]
-
-const components = [
-  ElScrollbar,
-  ElButton,
-  ElTag,
-  ElSpace,
-  ElDropdown,
-  ElDropdownItem,
-  ElDropdownMenu
-]
+// 完整引入element-plus
+import ElementPlus from 'element-plus'
 
 export const setupElementPlus = (app: App<Element>) => {
-  plugins.forEach((plugin) => {
-    app.use(plugin)
-  })
-
-  components.forEach((component) => {
-    app.component(component.name, component)
-  })
+  app.use(ElementPlus)
 }
