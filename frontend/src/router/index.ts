c18d053b435b71fb1bcedbbab3cfd67c5e1b918d
zhaoxu <1162291474@qq.com>
2024-04-23 14:43:20 +0800

    增加base url路径

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,11 @@
 import type { App } from 'vue'
 import type { RouteRecordRaw } from 'vue-router'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import remainingRouter from './modules/remaining'
 
 // 创建路由实例
 const router = createRouter({
-  history: createWebHashHistory(), // createWebHashHistory URL带#，createWebHistory URL不带#
+  history: createWebHistory(import.meta.env.VITE_FRONT_BASE_URL), // createWebHashHistory URL带#，createWebHistory URL不带#
   strict: true,
   routes: remainingRouter as RouteRecordRaw[],
   scrollBehavior: () => ({ left: 0, top: 0 })
