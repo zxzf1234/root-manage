65f2041d195dcd4c8c1a57d1eb8100edbe6d87cb
zhaoxu <1162291474@qq.com>
2024-04-23 17:55:08 +0800

    测试url路径（4）

--- frontend/types/env.d.ts
+++ frontend/types/env.d.ts
@@ -22,7 +22,6 @@ interface ImportMetaEnv {
   readonly VITE_DROP_CONSOLE: string
   readonly VITE_SOURCEMAP: string
   readonly VITE_OUT_DIR: string
-  readonly VITE_FRONT_BASE_URL: string
 }
 
 declare global {
