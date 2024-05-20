0915037a144d64997ecd821fbfad8e84f7f21cec
zhaoxu <1162291474@qq.com>
2024-05-20 10:56:49 +0800

    优化升级文件

--- backend/yudao-service/yudao-service-biz/src/main/resources/db/migration/202405/18/V20240518_1_002.sql
+++ backend/yudao-service/yudao-service-biz/src/main/resources/db/migration/202405/18/V20240518_1_002.sql
@@ -2,6 +2,6 @@ insert ignore into INFRA_DATABASE_COLUMN(CREATOR_ID, UPDATER_ID, ID, TABLE_ID, C
 insert ignore into INFRA_DATABASE_INDEX(CREATOR_ID, UPDATER_ID, ID, TABLE_ID, INDEX_TYPE, INDEX_NAME, COLUMN_NAMES) values('1', '1', '5598bf5e-750d-4d48-b6a1-cfc6920da108', 'e1a06dd2-bba2-49f8-ae05-8c172f216d61', 'UNIQUE INDEX', 'UN_system_user', '["username","deletedTime"]');
 update INFRA_INTERFACE_VO_CLASS set CREATE_TIME = '2023-08-10T18:57:50', UPDATE_TIME = '2023-11-10T10:10:19', CREATOR_ID = '0', UPDATER_ID = '0', NAME = 'SystemUserBase', COMMENT = '用户信息表', TYPE = '0', PARENT_ID = 'e1a06dd2-bba2-49f8-ae05-8c172f216d61' where ID = '8b0b3235-376c-11ee-98db-00163e021dba';
 ALTER TABLE system_user
-ADD COLUMN `deletedTime` DATETIME NOT NULL DEFAULT 0 COMMENT '删除时间',
+ADD COLUMN `deletedTime` DATETIME DEFAULT NULL COMMENT '删除时间',
 ADD UNIQUE INDEX  `UN_system_user` (`username`,`deletedTime`),
 DROP INDEX `idx_username`;
