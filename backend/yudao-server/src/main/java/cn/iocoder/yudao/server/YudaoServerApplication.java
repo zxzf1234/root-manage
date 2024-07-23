0d21240ba31faa35d33e3f2db312c99634ff8e6a
zhaoxu <1162291474@qq.com>
2024-07-23 15:07:13 +0800

    删除banner模块

--- backend/yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
+++ backend/yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
@@ -1,8 +1,10 @@
 package cn.iocoder.yudao.server;
 
+import cn.hutool.core.thread.ThreadUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.babyfish.jimmer.spring.repository.EnableJimmerRepositories;
+import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("SpringComponentScan")
 @SpringBootApplication(scanBasePackages = {"cn.iocoder.yudao.server", "cn.iocoder.yudao.service"})
@@ -11,6 +13,12 @@ public class YudaoServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(YudaoServerApplication.class, args);
+        ThreadUtil.execute(() -> {
+            ThreadUtil.sleep(1, TimeUnit.SECONDS); // 延迟 1 秒，保证输出到结尾
+            System.out.println("----------------------------------------------------------\n\t" +
+                    "项目启动成功！\n" +
+                    "----------------------------------------------------------");
+        });
     }
 
 }
