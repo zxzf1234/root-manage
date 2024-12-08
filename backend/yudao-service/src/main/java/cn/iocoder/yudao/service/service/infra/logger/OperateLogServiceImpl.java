49ad35410841b0811562855b2296da961bc4a01f
zhaoxu <1162291474@qq.com>
2024-12-08 23:29:11 +0800

    优化日志的记录

--- backend/yudao-service/src/main/java/cn/iocoder/yudao/service/service/infra/logger/OperateLogServiceImpl.java
+++ backend/yudao-service/src/main/java/cn/iocoder/yudao/service/service/infra/logger/OperateLogServiceImpl.java
@@ -3,6 +3,7 @@ package cn.iocoder.yudao.service.service.infra.logger;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.iocoder.yudao.service.framework.web.web.core.pojo.PageResult;
+import cn.iocoder.yudao.service.model.infra.logger.SystemOperateLogProps;
 import cn.iocoder.yudao.service.util.string.StrUtils;
 import cn.iocoder.yudao.service.vo.infra.logger.operatelog.OperateLogCreateReqVO;
 import cn.iocoder.yudao.service.model.system.user.SystemUser;
@@ -15,6 +16,7 @@ import cn.iocoder.yudao.service.model.infra.logger.SystemOperateLogDraft;
 import cn.iocoder.yudao.service.repository.infra.logger.SystemOperateLogRepository;
 import cn.iocoder.yudao.service.service.system.user.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.babyfish.jimmer.ImmutableObjects;
 import org.babyfish.jimmer.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -45,7 +47,8 @@ public class OperateLogServiceImpl implements OperateLogService {
     public void createOperateLog(OperateLogCreateReqVO createReqDTO) {
         SystemOperateLog logDO = OperateLogConvert.INSTANCE.convert(createReqDTO);
         SystemOperateLog finalLogDO = logDO;
-        logDO = SystemOperateLogDraft.$.produce(logDO, draft -> draft.setJavaMethodArgs(StrUtils.maxLength(finalLogDO.javaMethodArgs(), JAVA_METHOD_ARGS_MAX_LENGTH))
+        logDO = SystemOperateLogDraft.$.produce(logDO, draft -> draft.setJavaMethodArgs(ImmutableObjects.isLoaded(finalLogDO, SystemOperateLogProps.JAVA_METHOD_ARGS) ?
+                        StrUtils.maxLength(finalLogDO.javaMethodArgs(), JAVA_METHOD_ARGS_MAX_LENGTH) : "")
                 .setResultData(StrUtils.maxLength(finalLogDO.resultData(), RESULT_MAX_LENGTH)));
         systemOperateLogRepository.insert(logDO);
     }
