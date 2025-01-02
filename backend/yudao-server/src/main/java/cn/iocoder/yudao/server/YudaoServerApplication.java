d0f5086cd7f5d18736c200359a570eb7cba19d3f
zhaoxu <1162291474@qq.com>
2025-01-02 21:01:41 +0800

    多版本优化（1）

--- backend/yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
+++ backend/yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
@@ -4,12 +4,15 @@ import cn.hutool.core.thread.ThreadUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.babyfish.jimmer.spring.repository.EnableJimmerRepositories;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("SpringComponentScan")
 @SpringBootApplication(scanBasePackages = {"cn.iocoder.yudao.server", "cn.iocoder.yudao.service"})
 @EnableJimmerRepositories(basePackages= {"cn.iocoder.yudao.service.repository"})
-public class YudaoServerApplication {
+public class YudaoServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(YudaoServerApplication.class, args);
@@ -19,6 +22,10 @@ public class YudaoServerApplication {
                     "项目启动成功！\n" +
                     "----------------------------------------------------------");
         });
+    }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(YudaoServerApplication.class);
     }
 
 }
