61ca13151888829a90ca880f43e395ff32d3ea44
zhaoxu <1162291474@qq.com>
2025-01-04 21:19:21 +0800

    多版本优化（3）

---
+++ backend/yudao-server/src/main/java/cn/iocoder/yudao/server/aop/serverAspect.java
@@ -0,0 +1,27 @@
+package cn.iocoder.yudao.server.aop;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.io.IOException;
+
+@Component
+public class serverAspect implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String uri = ((HttpServletRequest) request).getRequestURI();
+        System.out.println("uri is " + uri);
+        // 解析URL中的版本号
+        String pathInfo = httpRequest.getPathInfo();
+        System.out.println("pathInfo is " + pathInfo);
+        chain.doFilter(request, response);
+    }
+}
