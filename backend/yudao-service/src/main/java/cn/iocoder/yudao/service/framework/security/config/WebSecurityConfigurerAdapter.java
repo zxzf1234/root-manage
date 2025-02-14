eb4de2a062e8ad2a97f5440034226869dd875c93
zhaoxu <1162291474@qq.com>
2025-02-15 00:09:42 +0800

    多版本优化（21）

--- backend/yudao-service/src/main/java/cn/iocoder/yudao/service/framework/security/config/WebSecurityConfigurerAdapter.java
+++ backend/yudao-service/src/main/java/cn/iocoder/yudao/service/framework/security/config/WebSecurityConfigurerAdapter.java
@@ -120,7 +120,7 @@ public class WebSecurityConfigurerAdapter {
                 .authorizeHttpRequests(auth -> {
                     auth
                         // 1.1 静态资源，可匿名访问
-                        .requestMatchers(HttpMethod.GET, "/*.html", "/**/*.html", "/**/*.css", "/**/*.js").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/*/*.html", "/*/*.css", "/*/*.js").permitAll()
                         // 1.2 设置 @PermitAll 无需认证
                         .requestMatchers(HttpMethod.GET, permitAllUrls.get(HttpMethod.GET).toArray(new String[0])).permitAll()
                         .requestMatchers(HttpMethod.POST, permitAllUrls.get(HttpMethod.POST).toArray(new String[0])).permitAll()
