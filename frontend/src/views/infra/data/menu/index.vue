4a03f7e3ee7b8c90226798466bda0c470ced1b42
zhaoxu <1162291474@qq.com>
2025-04-30 23:54:03 +0800

    菜单管理页面刷新缓存权限失效

--- frontend/src/views/infra/data/menu/index.vue
+++ frontend/src/views/infra/data/menu/index.vue
@@ -177,6 +177,7 @@ const refreshMenu = async () => {
     await message.confirm('即将更新缓存刷新浏览器！', '刷新菜单缓存')
     // 清空，从而触发刷新
     wsCache.delete(CACHE_KEY.ROLE_ROUTERS)
+    wsCache.delete(CACHE_KEY.USER)
     // 刷新浏览器
     location.reload()
   } catch {}
