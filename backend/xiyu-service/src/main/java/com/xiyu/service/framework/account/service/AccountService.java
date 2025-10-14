72ce3a1fbe1155971bcc0ef12189d8086237ee9e
zhaoxu <1162291474@qq.com>
2025-10-14 09:59:54 +0800

    Reapply "优化拉取卖家账户信息的url"
    This reverts commit c9d5d2f7473ebde6581fddb843b2bd10dd1ea003.

--- backend/xiyu-service/src/main/java/com/xiyu/service/framework/account/service/AccountService.java
+++ backend/xiyu-service/src/main/java/com/xiyu/service/framework/account/service/AccountService.java
@@ -20,7 +20,7 @@ public class AccountService {
 
     List<AccountInfo> accountInfos = new ArrayList<>();
 
-    public AccountService(@Value("${xiyu.account-router_url}") String accountRouterUrl, @Value("${spring.application.number}") String applicationNo,
+    public AccountService(@Value("${xiyu.devops-account-info-url}") String accountRouterUrl, @Value("${spring.application.number}") String applicationNo,
                           @Value("${xiyu.server-env}") String serverEnv) throws Exception {
         if(Objects.equals(serverEnv, "local"))
             return;
